c32345aa9cae6245ab2ad0c6a9bc569712eb08d5
Miikka Silfverberg <mpsilfve@iki.fi>
2021-05-27 14:17:10 -0700

    Input the query from the commandline

--- source/query.py
+++ source/query.py
@@ -1,14 +1,19 @@
 from elasticsearch import Elasticsearch
+from sys import argv
+
+MAX_RES=15
 
 if __name__=="__main__":
     print("Initializing Elasticsearch...")
     es = Elasticsearch()
 
-    query = "vera0sfg3en"
-    print("Query",query)
-    dsl = {"track_total_hits":True, "query": {"regexp": { "text": ".*%s.*" % query }}}
-    
+    query = argv[1]
+    print("Query:",query)
+
+    dsl = {"track_total_hits":True, "size": MAX_RES, "query": {"regexp": { "text": ".*%s.*" % query }}}    
     res = es.search(dsl,index="bin")
+
     print("Got %d Hits:" % res['hits']['total']['value'])
-    for hit in res['hits']['hits']:
-        print(hit["_source"])
+    print("Displaying maximally %u results" % MAX_RES)
+    for i, hit in enumerate(res['hits']['hits']):
+        print("%u:\t%s" % (i+1,hit["_source"]["text"]))
