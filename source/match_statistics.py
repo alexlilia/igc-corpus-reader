d87e6076169cfed0f0f5229c42a4adcba1797409
Miikka Silfverberg <mpsilfve@iki.fi>
2021-06-14 19:22:20 -0700

    Write longest line to file

--- source/match_statistics.py
+++ source/match_statistics.py
@@ -2,10 +2,16 @@ import numpy
 from sys import stdin
 
 lengths = []
+longest = ""
+llen = 0
 
 for line in stdin:
     lengths.append(len(line) - 1)
+    if lengths[-1] > llen:
+        longest = line[:-1]
+        llen = lengths[-1]
 
+print(longest,file=open("longest_line.txt","w"))
 print("Average line length in chars: %.2f" % numpy.mean(lengths))
 print("Median line length in chars: %.2f" % numpy.median(lengths))
 print("Longest line in chars: %u" % max(lengths))
